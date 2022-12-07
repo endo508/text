0df33aa31c2687a203806bb3a2fe6ea972996999
endo508 <endo508@yahoo.co.jp>
2022-12-07 09:45:39 +0900

    12-07 変更

--- staff2/staff_add.php
+++ staff2/staff_add.php
@@ -4,22 +4,35 @@
 <head>
     <meta charset="utf-8">
     <title>ろくまる農園ver2</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-    スタッフ追加<br />
-    <br />
-    <form method="post" action="staff_add_check.php">
-        スタッフ名を入力してください。<br />
-        <input type="text" name="name" style="width:200px"><br />
-        パスワードを入力してください。<br />
-        <input type="password" name="pass" style="width:100px"><br />
-        パスワードをもう一度入力してください。<br />
-        <input type="password" name="pass2" style="width:100px"><br />
-        <br />
-        <button type="button" onclick="history.back()">戻る</button>
-        <button type="submit">ＯＫ</button>
-    </form>
+    <h1>スタッフ追加</h1>
+
+    <main>
+        <form method="post" action="staff_add_check.php">
+            <p>スタッフの情報を入力します</p>
+            <table>
+                <tr>
+                    <td>スタッフ名</td>
+                    <td class="haba2"><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td>パスワード</td>
+                    <td><input type="password" name="pass"></td>
+                </tr>
+                <tr>
+                    <td>パスワード（確認）</td>
+                    <td><input type="password" name="pass2"></td>
+                </tr>
+            </table>
+            <div class="buttons">
+                <button type="button" onclick="history.back()">戻る</button>
+                <button class="button2" type="submit">ＯＫ</button>
+            </div>
+        </form>
+    </main>
 </body>
 
 </html>
